3e1b3c713b7865a82b916a630fca0034dc0edb1c
Eidolf <andreas@eidolf.de>
2026-04-21 09:14:31 +0000

    fix: ignore auxiliary coordinator errors during startup to prevent ConfigEntryNotReady

--- custom_components/hoymiles_wifi/coordinator.py
+++ custom_components/hoymiles_wifi/coordinator.py
@@ -159,12 +159,12 @@ class HoymilesConfigUpdateCoordinator(HoymilesDataUpdateCoordinator):
         try:
             response = await self._dtu.async_get_config()
         except Exception as err:
-            self._handle_update_failure("Config")
-            return
+            _LOGGER.debug("Config: Exception while querying data: %s", err)
+            return None
 
         if not response:
-            self._handle_update_failure("Config")
-            return
+            _LOGGER.debug("Unable to retrieve config data. Inverter might be offline.")
+            return None
 
         self._handle_update_success("Config")
         return response
@@ -184,12 +184,12 @@ class HoymilesAppInfoUpdateCoordinator(HoymilesDataUpdateCoordinator):
         try:
             response = await self._dtu.async_app_information_data()
         except Exception as err:
-            self._handle_update_failure("AppInfo")
-            return
+            _LOGGER.debug("AppInfo: Exception while querying data: %s", err)
+            return None
 
         if not response:
-            self._handle_update_failure("AppInfo")
-            return
+            _LOGGER.debug("Unable to retrieve app info data. Inverter might be offline.")
+            return None
 
         # Encryption handling only runs on successful responses
         if response.dtu_info.dfs:
